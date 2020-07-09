a0dcbd6da02a92c4ea7fcee19fbd43296be4363f
nyy28477590 <nyy28477590@gmail.com>
2020-07-09 20:48:04 +0800

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace 抽籤
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.seatNo = new System.Windows.Forms.Label();
+            this.groupNo = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnTeacher = new System.Windows.Forms.Button();
+            this.btnStaff = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1920, 400);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::抽籤.Properties.Resources.主圖;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 406);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(1920, 468);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // seatNo
+            // 
+            this.seatNo.AutoSize = true;
+            this.seatNo.Font = new System.Drawing.Font("微軟正黑體", 100F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.seatNo.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.seatNo.Location = new System.Drawing.Point(295, 224);
+            this.seatNo.Name = "seatNo";
+            this.seatNo.Size = new System.Drawing.Size(689, 251);
+            this.seatNo.TabIndex = 0;
+            this.seatNo.Text = "label1";
+            // 
+            // groupNo
+            // 
+            this.groupNo.AutoSize = true;
+            this.groupNo.Font = new System.Drawing.Font("微軟正黑體", 100F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.groupNo.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.groupNo.Location = new System.Drawing.Point(295, 475);
+            this.groupNo.Name = "groupNo";
+            this.groupNo.Size = new System.Drawing.Size(689, 251);
+            this.groupNo.TabIndex = 4;
+            this.groupNo.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("微軟正黑體", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(673, 880);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(638, 132);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "按空白鍵抽籤";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.BackColor = System.Drawing.SystemColors.ControlText;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("微軟正黑體", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Location = new System.Drawing.Point(612, 880);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(734, 132);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "按空白鍵回首頁";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // btnTeacher
+            // 
+            this.btnTeacher.AutoSize = true;
+            this.btnTeacher.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnTeacher.Location = new System.Drawing.Point(1781, 912);
+            this.btnTeacher.Name = "btnTeacher";
+            this.btnTeacher.Size = new System.Drawing.Size(92, 33);
+            this.btnTeacher.TabIndex = 5;
+            this.btnTeacher.TabStop = false;
+            this.btnTeacher.Text = "老師抽籤";
+            this.btnTeacher.UseVisualStyleBackColor = true;
+            this.btnTeacher.Click += new System.EventHandler(this.btnTeacher_Click);
+            // 
+            // btnStaff
+            // 
+            this.btnStaff.AutoSize = true;
+            this.btnStaff.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnStaff.Location = new System.Drawing.Point(1781, 965);
+            this.btnStaff.Name = "btnStaff";
+            this.btnStaff.Size = new System.Drawing.Size(92, 33);
+            this.btnStaff.TabIndex = 6;
+            this.btnStaff.TabStop = false;
+            this.btnStaff.Text = "工作人員";
+            this.btnStaff.UseVisualStyleBackColor = true;
+            this.btnStaff.Click += new System.EventHandler(this.btnStaff_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1898, 1024);
+            this.Controls.Add(this.btnStaff);
+            this.Controls.Add(this.btnTeacher);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupNo);
+            this.Controls.Add(this.seatNo);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label seatNo;
+        private System.Windows.Forms.Label groupNo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnTeacher;
+        private System.Windows.Forms.Button btnStaff;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace 抽籤
+{
+    public partial class Form1 : Form
+    {
+        int[] randon = new int[31] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 };
+        int[] teacher = new int[4] { 1, 2, 3, 4 };
+        int[] staff = new int[3] { 1, 8, 16 };
+        int count = 0;
+        int countThree = 0;
+        int countStaff = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //int[] randon = new int[31];
+
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            { System.Media.SoundPlayer backgroundMusic = new System.Media.SoundPlayer(@"D:\商務謝師夜\抽籤背景音樂.wav");
+              backgroundMusic.PlayLooping();
+            }
+            catch
+            {
+                MessageBox.Show("確認音效位址");
+            }
+
+            seatNo.Visible = false;
+            groupNo.Visible = false;
+            button2.Visible = false;
+
+            Thread thread = new Thread(new ThreadStart(ColorChange));
+            thread.Start();
+
+
+            Random rnd = new Random(); //產生亂數初始值
+            Random rdThree = new Random();
+            Random rdStaff = new Random();
+
+            for (int i = 0; i < randon.Length; i++)
+            {
+                int randomIndex = rnd.Next(randon.Length);
+                int tempIndex = randon[randomIndex];
+                randon[randomIndex] = randon[i];
+                randon[i] = tempIndex;
+            }
+
+            for (int j = 0; j<teacher.Length; j++)
+            {
+                int threeIndex = rdThree.Next(teacher.Length);
+                int tmpThreeIndex = teacher[threeIndex];
+                teacher[threeIndex] = teacher[j];
+                teacher[j] = tmpThreeIndex;
+            }
+
+            for (int z = 0; z < staff.Length; z++)
+            {
+                int staffIndex = rdThree.Next(staff.Length);
+                int tmpStaffIndex = staff[staffIndex];
+                staff[staffIndex] = staff[z];
+                staff[z] = tmpStaffIndex;
+            }
+        }
+
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                System.Media.SoundPlayer startMusic = new System.Media.SoundPlayer(@"D:\商務謝師夜\開始抽籤.wav");
+                startMusic.Play();
+            }
+            catch
+            {
+                MessageBox.Show("確認音效位址");
+            }
+
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = false;
+            seatNo.Visible = true;
+            groupNo.Visible = true;
+            button2.Visible = true;
+            button1.Visible = false;
+
+            Thread thread1 = new Thread(new ThreadStart(ColorChange));
+            thread1.Start();
+            thread1.Abort();
+
+
+            try
+            {
+                int seat = randon[count];
+
+                if(seat == 1 || seat == 9 || seat == 17)
+                {
+                    count++;
+                    seat = randon[count];
+                }
+                seatNo.Text = "座位號碼：" +seat.ToString();
+                count++;
+
+                if (seat <= 8)
+                {
+                    groupNo.Text = "分組組別：A";
+                }
+                else if (seat <= 16)
+                {
+                    groupNo.Text = "分組組別：B";
+                }
+                else if (seat <= 24)
+                {
+                    groupNo.Text = "分組組別：C";
+                }
+                else
+                {
+                    groupNo.Text = "分組組別：D";
+                }
+            }
+            catch
+            {
+                MessageBox.Show("全員到齊！", "恭喜", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                System.Media.SoundPlayer backgroundMusic = new System.Media.SoundPlayer(@"D:\商務謝師夜\抽籤背景音樂.wav");
+                backgroundMusic.PlayLooping();
+            }
+            catch
+            {
+                MessageBox.Show("確認音效位址");
+            }
+
+            pictureBox1.Visible = true;
+            pictureBox2.Visible = true;
+            button1.Visible = true;
+            seatNo.Visible = false;
+            groupNo.Visible = false;
+            button2.Visible = false;
+            btnTeacher.Visible = true;
+            btnStaff.Visible = true;
+
+            Thread thread2 = new Thread(new ThreadStart(ColorChange));
+            thread2.Start();
+            thread2.Abort();
+        }
+
+        private void ColorChange()
+        {
+            Random rndColor = new Random();
+            while(this.button1.Visible == true || this.button2.Visible == true)
+            {
+                int red = rndColor.Next(1, 256);
+                int green = rndColor.Next(1, 256);
+                int blue = rndColor.Next(1, 256);
+                this.button1.ForeColor = Color.FromArgb(red, green, blue);
+                this.button2.ForeColor = Color.FromArgb(red, green, blue);
+                Thread.Sleep(500);
+            }
+        }
+
+        private void btnTeacher_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                System.Media.SoundPlayer startMusic = new System.Media.SoundPlayer(@"D:\商務謝師夜\開始抽籤.wav");
+                startMusic.Play();
+            }
+            catch
+            {
+                MessageBox.Show("確認音效位址");
+            }
+
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = false;
+            seatNo.Visible = true;
+            groupNo.Visible = true;
+            button2.Visible = true;
+            button1.Visible = false;
+            btnTeacher.Visible = false;
+            btnStaff.Visible = false;
+
+            Thread thread = new Thread(new ThreadStart(ColorChange));
+            thread.Start();
+            thread.Abort();
+
+
+            try
+            {
+                int seat = teacher[countThree];
+
+                seatNo.Text = "座位號碼：" + seat.ToString();
+                countThree++;
+
+                if (seat == 1)
+                {
+                    groupNo.Text = "分組組別：A";
+                }
+                else if (seat == 2)
+                {
+                    groupNo.Text = "分組組別：B";
+                }
+                else if (seat == 3)
+                {
+                    groupNo.Text = "分組組別：C";
+                }
+                else
+                {
+                    groupNo.Text = "分組組別：D";
+                }
+            }
+            catch
+            {
+                MessageBox.Show("全員到齊！", "恭喜", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+        }
+
+        private void btnStaff_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                System.Media.SoundPlayer startMusic = new System.Media.SoundPlayer(@"D:\商務謝師夜\開始抽籤.wav");
+                startMusic.Play();
+            }
+            catch
+            {
+                MessageBox.Show("確認音效位址");
+            }
+
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = false;
+            seatNo.Visible = true;
+            groupNo.Visible = true;
+            button2.Visible = true;
+            button1.Visible = false;
+            btnTeacher.Visible = false;
+            btnStaff.Visible = false;
+
+            Thread thread = new Thread(new ThreadStart(ColorChange));
+            thread.Start();
+            thread.Abort();
+
+
+            try
+            {
+                int seat = staff[countStaff];
+
+                seatNo.Text = "座位號碼：" + seat.ToString();
+                countStaff++;
+
+                if (seat == 1)
+                {
+                    groupNo.Text = "分組組別：A";
+                }
+                else if (seat == 8)
+                {
+                    groupNo.Text = "分組組別：B";
+                }
+                else if (seat == 16)
+                {
+                    groupNo.Text = "分組組別：C";
+                }
+                else
+                {
+                    groupNo.Text = "分組組別：D";
+                }
+            }
+            catch
+            {
+                MessageBox.Show("全員到齊！", "恭喜", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+        }
+    
+    }
+}
